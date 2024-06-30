f93efd48c26f49bbe9f63310f72f4c7a1d3d129f
Antonio Alvarado <antoniojalvarado53@gmail.com>
2024-06-30 01:10:03 -0400

    added modal automatic modification on champion-click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
+//element inicialization
+
 const champRow = document.getElementById("champ-row")
+const modalContainer = document.getElementById("modal-container")
+
+
+//render champion cards
 
 const renderChampions = async () =>{
     try{
@@ -8,8 +14,8 @@ const renderChampions = async () =>{
 
             for(let champ in data.data){
                 champRow.innerHTML += `
-      <div class="col-3 d-flex justify-content-center myCard align-items-center">
-        <div class="champCard" id="${data.data[champ].name}">
+      <div class="col-3 d-flex justify-content-center card-frame align-items-center">
+        <div class="champCard" id="${data.data[champ].id}">
           <img src="${`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.data[champ].id}_0.jpg`}">
           <div class="cardContent">
             <p class="champ-name">${data.data[champ].name.toUpperCase()}</p>
@@ -30,15 +36,59 @@ const renderChampions = async () =>{
     }
 }
 
-const renderModal = () =>{
-  const modalContainer = document.getElementById("modal-container");
-  modalContainer.innerHTML = `<div class="champ-modal">
+//render champion modal
 
-  </div>`
+const renderChampionModal = async (champId) =>{
+  try{
+    const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/14.13.1/data/en_US/champion/${champId}.json`)
+    if(response.status == 200){
+      
+      const data = await response.json()
+      
+      console.log(data.data[champId].name)
+
+      modalContainer.innerHTML = `
+      <div class="modal-background">
+    <div class="champ-modal">
+      <h3 class="champ-modal-title">${data.data[champId].title}</h3>
+      <h1 class="champ-modal-name">${data.data[champId].name}</h1>
+      <a class="modal-exit">X</a>
+      <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champId}_0.jpg" class="champ-full-splash">
+      <div class="champ-skills">
+        <div class="champ-skill"><img id ="Passive" class="champ-skill-img" src="https://ddragon.leagueoflegends.com/cdn/14.13.1/img/passive/${data.data[champId].passive.image.full}"></div>
+        <div class="champ-skill"><img id ="Q"  class="champ-skill-img clickeable-skill" src="https://ddragon.leagueoflegends.com/cdn/14.13.1/img/spell/${data.data[champId].spells[0].image.full}"></div>
+        <div class="champ-skill"><img id ="W"  class="champ-skill-img clickeable-skill" src="https://ddragon.leagueoflegends.com/cdn/14.13.1/img/spell/${data.data[champId].spells[1].image.full}"></div>
+        <div class="champ-skill"><img id ="E"  class="champ-skill-img clickeable-skill" src="https://ddragon.leagueoflegends.com/cdn/14.13.1/img/spell/${data.data[champId].spells[2].image.full}"></div>
+        <div class="champ-skill"><img id ="R"  class="champ-skill-img clickeable-skill" src="https://ddragon.leagueoflegends.com/cdn/14.13.1/img/spell/${data.data[champId].spells[3].image.full}"></div>
+      </div>
+      <h1 class="modal-skill-title">Passive - ${data.data[champId].passive.name}</h1>
+      <p class="modal-skill-text">${data.data[champId].passive.description}</p>
+    </div>
+  </div>
+      `
+    }
+    else{
+      throw new Error('failed to fetch data')
+    }
+  }
+  catch(error){
+    console.log(error)
+  }
 }
 
+
+//card event listener that render modal
+
 champRow.addEventListener('click', (e)=>{
-  console.log(e.target.closest(".champCard").id)
+  if(e.target.classList.contains('card-frame')){
+    return
+  }
+  else{
+    renderChampionModal(e.target.closest(".champCard").id)
+  }
+  
 })
+
+//callings
 
 renderChampions()
